b105f038805d2831d3a74015abb2aa53ced0e245
Nafiz Al Turabi <nafizalturabi18cse@gmail.com>
2026-05-06 17:04:20 +0600

    Project card added

---
+++ app/(Dashboard)/dashboard/projects/page.tsx
@@ -0,0 +1,10 @@
+import Projects from '@/components/DashboardComp/Projects/Projects'
+import React from 'react'
+
+export default function ProjectsDashboardPage() {
+  return (
+    <div>
+        <Projects/>
+    </div>
+  )
+}
---
+++ components/DashboardComp/Projects/Projects.tsx
@@ -0,0 +1,30 @@
+import Image from 'next/image';
+import React from 'react';
+import { FaPlus } from 'react-icons/fa';
+
+const Projects = () => {
+    return (
+        <div>
+            <header className='flex items-center justify-between gap-4 '>
+                <h1 className="text-2xl font-bold mb-4 custom-font">Projects</h1>
+                <button className='flex items-center gap-2 font-bold custom-font py-2 px-4 border border-slate-300 dark:border-slate-700 dark:bg-[#0f172b] dark:text-white uppercase rounded-full'> <FaPlus />Add</button>
+            </header>
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4  '>
+                <div className='bg-white dark:bg-[#0f172b] p-4 rounded-2xl'>
+                    <Image height={400} width={400} src="/card.webp" alt="" className='h-56 object-cover rounded-2xl' />
+                    <div>
+                        <h1 className='text-xl font-bold mt-4'>Project Title</h1>
+                        <p className=''>
+                            This is a brief description of the project. It provides an overview of what the project entails and its key features.
+                        </p>
+                    </div>
+                    <div>
+                        <button></button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Projects;
